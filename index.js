e009ef637759af87e7b5580d9007ccd788b5e48f
Hoko Le <hokole527@gmail.com>
2019-08-16 18:13:11 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,36 @@
+var a, b 
+
+function add(a, b) {
+  var result = a + b 
+  return result
+}
+
+function subtract(a, b) {
+  var result = a - b 
+  return result
+}
+
+function multiply(a, b) {
+  var result = a * b 
+  return result
+}
+
+function divide(a, b) {
+  var result = a / b 
+  return result
+}
+
+function inc(n) {
+  var result = n++
+  return result
+}
+
+function dec(n) {
+  var result = n--
+  return result
+}
+
+function makeInt(n) {
+  parseInt(n)
+  return n
+}
